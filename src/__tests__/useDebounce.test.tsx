00bc148a64fd67e6785ad5809539214bb3db6695
sam1362 <150675656+sam1362@users.noreply.github.com>
2025-11-30 13:00:50 +0100

    usedebouncetest,300

--- src/__tests__/useDebounce.test.tsx
+++ src/__tests__/useDebounce.test.tsx
@@ -6,9 +6,9 @@ import { useDebounce } from '../hooks/useDebounce'
 jest.useFakeTimers()
 
 describe('useDebounce', () => {
-  // Verifiserer at verdien oppdateres etter 600 ms
+  // Verifiserer at verdien oppdateres etter 300 ms
   it('debouncer oppdaterer etter 600ms', () => {
-    const { result, rerender } = renderHook(({ value }) => useDebounce(value, 600), {
+    const { result, rerender } = renderHook(({ value }) => useDebounce(value, 300), {
       initialProps: { value: 'Oslo' },
     })
 
@@ -18,7 +18,7 @@ describe('useDebounce', () => {
     expect(result.current).toBe('Oslo')
 
     act(() => {
-      jest.advanceTimersByTime(600)
+      jest.advanceTimersByTime(300)
     })
 
     expect(result.current).toBe('Bergen')
